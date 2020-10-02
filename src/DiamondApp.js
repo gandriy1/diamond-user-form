745991158ef028805ea8ee4565fee1bdebcd3859
Guzenko, Andriy <andriy.guzenko@ceridian.com>
2020-10-02 01:24:33 -0400

    Added info alert and submission and split into react components

--- src/DiamondApp.js
+++ src/DiamondApp.js
@@ -1,55 +1,97 @@
 import React from 'react';
-//import logo from './logo.svg';
 import './App.css';
-import {Form, Button, Container} from 'react-bootstrap';
+import {Form, Button, Container, Alert} from 'react-bootstrap';
 import axios from 'axios';
 
 //show success message, explore this.func and const a = () => {}
 //figure out url config file for deployments
+//loading screen while submitting
 
 axios.defaults.baseURL = 'https://diamond-server.azurewebsites.net';
 
-function App() {
+function ClientForm(props){
+  //const emptyClientInfo = {name:'', address:'', phone: '', extraInfo:''};
+  const emptyClientInfo = {name:'Josh', address:'19 Mb', phone: '647', extraInfo:'none'};
 
-  const [clientName, setClientName] = React.useState('');
-  const [clientAddress, setClientAddress] = React.useState('');
+  let [clientInfo, setClientInfo] = React.useState(emptyClientInfo);
 
   const submitClientForm = (event) => {
     event.preventDefault();
-    let clientData = {name:clientName, address: clientAddress};
-    console.log(clientData);
+    props.onFinish(clientInfo);
+  }
+  
+  const setAttrValue = (attrName) => (event) => { 
+    setClientInfo({...clientInfo, [attrName]: event.target.value}); 
+  }
 
-    setClientName("");
-    setClientAddress("");
+  return (
+      <Form onSubmit={submitClientForm}>
+        <Form.Group controlId="formName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Name" value={clientInfo.name} onChange={setAttrValue("name")} />
+        </Form.Group>
 
-    axios.post('/addClient', clientData)
+        <Form.Group controlId="formAddress">
+          <Form.Label>Address</Form.Label>
+          <Form.Control as="textarea" placeholder="Address or intersection" value={clientInfo.address} onChange={setAttrValue("address")}/>
+        </Form.Group>
+
+        <Form.Group controlId="formPhone">
+          <Form.Label>Phone</Form.Label>
+          <Form.Control type="number" placeholder="Phone #" value={clientInfo.phone}  onChange={setAttrValue("phone")}/>
+        </Form.Group>
+
+        <Form.Group controlId="formExtraInfo">
+          <Form.Label>Extra Info</Form.Label>
+          <Form.Control as="textarea" placeholder="Email, type of work, visit date" value={clientInfo.extraInfo} onChange={setAttrValue("extraInfo")}/>
+        </Form.Group>
+
+        <div className="float-right">
+          <Button variant="primary" type="submit">Submit</Button>
+        </div>
+      </Form>
+  );
+}
+
+function SubmitStatus(props){
+  return (
+    <Alert variant="success">
+      <Alert.Heading>Client {props.clientName} was added</Alert.Heading>
+        <hr />
+        <div className="d-flex justify-content-end">
+          <Button onClick={props.onFinish} variant="outline-success">Back</Button>
+        </div>
+    </Alert>
+  );
+}
+
+let nameOfAddedClient = "";
+function App() {
+  let [showFormSubmittedStatus, setShowFormSubmittedStatus] = React.useState(false);
+
+  const processFormData = (clientInfo) => {
+    let serverReqBody = {name:clientInfo.name, address: clientInfo.address, phone: clientInfo.phone, extraInfo: clientInfo.extraInfo};
+    console.log(serverReqBody);
+
+    nameOfAddedClient =  clientInfo.name;
+    setShowFormSubmittedStatus(true);
+
+    axios.post('/addClient', serverReqBody)
     .then(function (response) {
       console.log(response);
     })
     .catch(function (error) {
       console.log(error);
     });
+  }
 
-    setClientName("");
-    setClientAddress("");
+  const backToForm = () => {
+    setShowFormSubmittedStatus(false);
   }
 
   return (
     <Container className="p-3">
-      <Form onSubmit={submitClientForm}>
-        <Form.Group controlId="formBasicName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter your name" value={clientName} onChange={event => setClientName(event.target.value)} />
-        </Form.Group>
-
-        <Form.Group controlId="formBasicAddress">
-          <Form.Label>Address or Intersection</Form.Label>
-          <Form.Control as="textarea" placeholder="Enter your address or intersection" value={clientAddress} onChange={event => setClientAddress(event.target.value)}/>
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+      { showFormSubmittedStatus ? <SubmitStatus onFinish={backToForm} clientName={nameOfAddedClient} /> : <ClientForm onFinish={processFormData}/> }
     </Container>
   );
 }
